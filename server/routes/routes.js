e1210d109e73dfe4fbb2623b3df99d30b8b1fded
kwanele <kwanelegamedze4@gmail.com>
2021-10-03 23:07:13 +0200

    added all routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const route = express.Router();
 const User = require("../schema/Shema");
 
-route.post("/", async (req, res) => {
+//creating user
+route.post("/create", async (req, res) => {
   try {
     const newUser = new User({ name: req.body.name, friend: req.body.friend });
     const user = await newUser.save().then(res.status(200).json("user added"));
@@ -12,6 +13,36 @@ route.post("/", async (req, res) => {
   }
 });
 
+//getting individual User
+route.get("/:id", async (req, res) => {
+  try {
+    User.findById(req.params.id).then((user) => res.json(user));
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//editing individual User
+route.put("/:id", async (req, res) => {
+  try {
+    User.findByIdAndUpdate(req.params.id, req.body).then(
+      res.json("User Updated")
+    );
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Deleting User
+route.delete("/:id", async (req, res) => {
+  try {
+    User.findByIdAndDelete(req.params.id).then(res.json("User Deleted"));
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Getting all Users
 route.get("/", async (req, res) => {
   try {
     User.find().then((users) => res.json(users));
