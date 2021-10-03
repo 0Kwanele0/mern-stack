9bc8f8a835c8955f06be09908a1d8888ca7601ef
kwanele <kwanelegamedze4@gmail.com>
2021-10-03 23:16:04 +0200

    correcte db string

--- server/db/db.js
+++ server/db/db.js
@@ -5,13 +5,10 @@ dotenv.config();
 const db = async () => {
   try {
     mongodb
-      .connect(
-        "mongodb+srv://kwanele:kwanele@cluster0.tokqm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-        {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        }
-      )
+      .connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
       .then(console.log("connected to db"));
   } catch (err) {
     console.log(err);
